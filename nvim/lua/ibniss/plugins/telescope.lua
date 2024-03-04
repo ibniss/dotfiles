84612d8ca8f7e3da94b069c89ac446549ef3a84c
Krzysztof Bielikowicz <krzysztof@causalens.com>
2024-03-04 02:06:57 +0000

    Telescope: add native fzf, ui-select

--- nvim/lua/ibniss/plugins/telescope.lua
+++ nvim/lua/ibniss/plugins/telescope.lua
@@ -1,17 +1,36 @@
 return {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.5',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    event = 'VimEnter',
+    branch = '0.1.x',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        {
+            -- fzf native extension for perf
+            'nvim-telescope/telescope-fzf-native.nvim',
+            build = 'make',
+        },
+        { 'nvim-telescope/telescope-ui-select.nvim' },
+        { 'nvim-tree/nvim-web-devicons' },
+    },
     config = function()
+        require('telescope').setup({
+            extensions = {
+                ['ui-select'] = {
+                    require('telescope.themes').get_dropdown(),
+                },
+            },
+        })
+
+        pcall(require('telescope').load_extension, 'fzf')
+        pcall(require('telescope').load_extension, 'ui-select')
+
         local builtin = require('telescope.builtin')
         --- Project Files (all)
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
         --- Git Files
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
         --- Project Search
-        vim.keymap.set('n', '<leader>ps', function()
-            builtin.grep_string({ search = vim.fn.input('Grep > ') })
-        end)
+        vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
         -- word search under cursor
         vim.keymap.set('n', '<leader>pws', function()
             builtin.grep_string({ search = vim.fn.expand('<cword>') })
