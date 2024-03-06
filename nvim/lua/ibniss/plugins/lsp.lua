ecabf1e9df3020c172c32226ae1a7d29fe381ac3
Krzysztof Bielikowicz <kbielikowicz98@gmail.com>
2024-03-06 01:54:38 +0000

    Add autotag, dressing; styling tweaks for lualine

--- nvim/lua/ibniss/plugins/lsp.lua
+++ nvim/lua/ibniss/plugins/lsp.lua
@@ -15,15 +15,6 @@ return {
         lazy = false,
         config = true,
     },
-    -- status updates for LSPs
-    {
-        'j-hui/fidget.nvim',
-        opts = {
-            notification = {
-                window = { winblend = 0 },
-            },
-        },
-    },
     -- Autocompletion
     {
         'hrsh7th/nvim-cmp',
--- nvim/lua/ibniss/plugins/lualine.lua
+++ nvim/lua/ibniss/plugins/lualine.lua
@@ -3,22 +3,45 @@ local LualineUtil = require('ibniss.util.lualine')
 return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
+    event = 'VeryLazy',
+    init = function()
+        vim.g.lualine_laststatus = vim.o.laststatus
+        if vim.fn.argc(-1) > 0 then
+            -- set an empty statusline till lualine loads
+            vim.o.statusline = ' '
+        else
+            -- hide the statusline on the starter page
+            vim.o.laststatus = 0
+        end
+    end,
     config = function()
         require('lualine').setup({
             theme = 'auto',
             sections = {
                 lualine_a = { 'mode' },
-                lualine_b = { '%{FugitiveStatusline()}', 'diff' },
+                lualine_b = { '%{FugitiveStatusline()}' },
                 lualine_c = {
                     LualineUtil.root_dir(),
                     {
                         'diagnostics',
                     },
+                    {
+                        'filetype',
+                        icon_only = true,
+                        separator = '',
+                        padding = { left = 1, right = 0 },
+                    },
                     { LualineUtil.pretty_path() },
                 },
 
-                lualine_x = { 'filetype' },
-                lualine_y = { 'progress' },
+                lualine_x = { 'diff' },
+                lualine_y = {
+                    {
+                        'progress',
+                        separator = ' ',
+                        padding = { left = 1, right = 0 },
+                    },
+                },
                 lualine_z = { 'location' },
             },
         })
