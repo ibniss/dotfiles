5b7fac47d242e20146ce45acb6a304f15453aa2d
Krzysztof Bielikowicz <krzysztof@causalens.com>
2024-09-05 21:33:19 +0100

    Switch from fugitive to neogit + diffview, update lualine

--- nvim/lua/ibniss/plugins/treesitter.lua
+++ nvim/lua/ibniss/plugins/treesitter.lua
@@ -11,6 +11,7 @@ return {
             require('nvim-treesitter.configs').setup({
                 -- A list of parser names, or "all" (the five listed parsers should always be installed)
                 ensure_installed = {
+                    'diff',
                     'javascript',
                     'typescript',
                     'tsx',
