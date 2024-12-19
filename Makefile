846ef027ff9649b49c2c102a44ad338959504879
Krzysztof Bielikowicz <krzysztof@causalens.com>
2024-12-19 18:24:34 +0100

    Bump packages, some setup for ocaml via dune-preview

--- Makefile
+++ Makefile
@@ -5,3 +5,9 @@ link-nvim:
 link-wezterm:
 	rm ~/.wezterm.lua
 	ln -s $(PWD)/wezterm ~/.config/wezterm
+
+link-zsh:
+	rm ~/.zshrc
+	ln -s $(PWD)/zsh/.zshrc ~/.zshrc
+	rm ~/.zprofile
+	ln -s $(PWD)/zsh/.zsh_profile ~/.zprofile
--- nvim/lua/ibniss/plugins/lsp.lua
+++ nvim/lua/ibniss/plugins/lsp.lua
@@ -1,54 +1,18 @@
 return {
-    {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v3.x',
-        lazy = true,
-        config = false,
-        init = function()
-            -- Disable automatic setup, we are doing it manually
-            vim.g.lsp_zero_extend_cmp = 0
-            vim.g.lsp_zero_extend_lspconfig = 0
-        end,
-    },
-    {
-        'williamboman/mason.nvim',
-        lazy = false,
-        config = true,
-    },
     -- Autocompletion
     {
         'hrsh7th/nvim-cmp',
         event = 'InsertEnter',
         dependencies = {
-            {
-                'L3MON4D3/LuaSnip',
-                build = (function()
-                    -- Build Step is needed for regex support in snippets
-                    -- This step is not supported in many windows environments
-                    -- Remove the below condition to re-enable on windows
-                    if
-                        vim.fn.has('win32') == 1
-                        or vim.fn.executable('make') == 0
-                    then
-                        return
-                    end
-                    return 'make install_jsregexp'
-                end)(),
-            },
             { 'hrsh7th/cmp-buffer' }, -- Optional
             { 'hrsh7th/cmp-path' }, -- Optional
             { 'hrsh7th/cmp-cmdline' }, -- Cmdline completions
-            { 'saadparwaiz1/cmp_luasnip' }, -- Optional
             { 'hrsh7th/cmp-nvim-lua' }, -- Optional
+            { 'hrsh7th/cmp-nvim-lsp' },
             -- icons for completion items
             { 'onsails/lspkind.nvim' },
         },
         config = function()
-            -- Here is where you configure the autocompletion settings.
-            local lsp_zero = require('lsp-zero')
-            local luasnip = require('luasnip')
-            lsp_zero.extend_cmp()
-
             -- And you can configure cmp even more, if you want to.
             local cmp = require('cmp')
             local cmp_select = { behavior = cmp.SelectBehavior.Select }
@@ -58,9 +22,6 @@ return {
             local lspkind = require('lspkind')
 
             cmp.setup({
-                snippet = {
-                    expand = function(args) luasnip.lsp_expand(args.body) end,
-                },
                 preselect = 'item', -- Automatically select the first item
                 completion = {
                     completeopt = 'menu,menuone,noinsert',
@@ -90,20 +51,7 @@ return {
                     ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
                     ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
                     ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-                    ['<C-e>'] = lsp_zero.cmp_action().toggle_completion(),
                     ['<C-Space>'] = cmp.mapping.complete(),
-                    -- <c-l> will move you to the right of each of the expansion locations.
-                    -- <c-h> is similar, except moving you backwards.
-                    ['<C-l>'] = cmp.mapping(function()
-                        if luasnip.expand_or_locally_jumpable() then
-                            luasnip.expand_or_jump()
-                        end
-                    end, { 'i', 's' }),
-                    ['<C-h>'] = cmp.mapping(function()
-                        if luasnip.locally_jumpable(-1) then
-                            luasnip.jump(-1)
-                        end
-                    end, { 'i', 's' }),
                 }),
                 -- add border to the completion window
                 window = {
@@ -140,96 +88,216 @@ return {
         cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
         event = { 'BufReadPre', 'BufNewFile' },
         dependencies = {
-            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'williamboman/mason.nvim' },
             { 'williamboman/mason-lspconfig.nvim' },
-            { 'folke/neodev.nvim', opts = {} },
+            { 'WhoIsSethDaniel/mason-tool-installer.nvim' },
+            {
+                -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
+                -- used for completion, annotations and signatures of Neovim apis
+                'folke/lazydev.nvim',
+                ft = 'lua',
+                opts = {
+                    library = {
+                        -- Load luvit types when the `vim.uv` word is found
+                        { path = 'luvit-meta/library', words = { 'vim%.uv' } },
+                    },
+                },
+            },
+            { 'Bilal2453/luvit-meta', lazy = true },
+
+            -- cmp for completions
+            { 'hrsh7th/cmp-nvim-lsp' },
         },
         config = function()
-            -- This is where all the LSP shenanigans will live
-            local lsp_zero = require('lsp-zero')
-            lsp_zero.extend_lspconfig()
+            -- local util to extend lspconfig
+            local extend = function(name, key, values)
+                local mod = require(string.format('lspconfig.configs.%s', name))
+                local default = mod.default_config
+                local keys = vim.split(key, '.', { plain = true })
+                while #keys > 0 do
+                    local item = table.remove(keys, 1)
+                    default = default[item]
+                end
 
-            --- if you want to know more about lsp-zero and mason.nvim
-            --- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
-            lsp_zero.on_attach(function(client, bufnr)
-                -- see :help lsp-zero-keybindings
-                -- to learn the available actions
-                local opts = { buffer = bufnr, remap = false }
-                vim.keymap.set(
-                    'n',
-                    'gd',
-                    require('telescope.builtin').lsp_definitions,
-                    opts
-                )
-                vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-                vim.keymap.set(
-                    'n',
-                    'gr',
-                    require('telescope.builtin').lsp_references,
-                    opts
-                )
-                vim.keymap.set(
-                    'n',
-                    'gI',
-                    require('telescope.builtin').lsp_implementations,
-                    opts
-                )
-                vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-                vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+                if vim.islist(default) then
+                    for _, value in ipairs(default) do
+                        table.insert(values, value)
+                    end
+                else
+                    for item, value in pairs(default) do
+                        if not vim.tbl_contains(values, item) then
+                            values[item] = value
+                        end
+                    end
+                end
+                return values
+            end
 
-                vim.keymap.set(
-                    'n',
-                    '<leader>e',
-                    vim.diagnostic.open_float,
-                    opts
-                )
-                vim.keymap.set(
-                    'n',
-                    '<leader>q',
-                    vim.diagnostic.setloclist,
-                    opts
-                )
-                vim.keymap.set(
-                    'n',
-                    '<leader>vca',
-                    vim.lsp.buf.code_action,
-                    opts
-                )
-                vim.keymap.set('n', '<leader>vrr', vim.lsp.buf.references, opts)
-                vim.keymap.set(
-                    'n',
-                    '<leader>vrn',
-                    function() return ':IncRename ' .. vim.fn.expand('<cword>') end,
-                    { expr = true }
-                )
+            local lspconfig = require('lspconfig')
 
-                --- Insert mode - C-H to show signature
-                vim.keymap.set('i', '<C-h>', vim.lsp.buf.signature_help, opts)
-            end)
-
-            require('mason-lspconfig').setup({
-                ensure_installed = {
-                    'tsserver',
-                    'lua_ls',
-                    'rust_analyzer',
-                    'basedpyright',
-                    'eslint',
+            -- server configurations
+            -- true means default configuration
+            local servers = {
+                lua_ls = true,
+                ts_ls = {
+                    root_dir = require('lspconfig').util.root_pattern(
+                        'package.json'
+                    ),
+                    single_file = false,
+                    server_capabilities = {
+                        documentFormattingProvider = false,
+                    },
                 },
-                handlers = {
-                    lsp_zero.default_setup,
-                    lua_ls = function()
-                        -- (Optional) Configure lua language server for neovim
-                        local lua_opts = lsp_zero.nvim_lua_ls()
-                        require('lspconfig').lua_ls.setup(lua_opts)
-                    end,
+                rust_analyzer = true,
+                basedpyright = true,
+                eslint = true,
+                ocamllsp = {
+                    manual_install = true,
+                    cmd = { 'dune', 'tools', 'exec', 'ocamllsp' },
+                    -- cmd = { "dune", "exec", "ocamllsp" },
+                    settings = {
+                        codelens = { enable = true },
+                        inlayHints = { enable = true },
+                        syntaxDocumentation = { enable = true },
+                    },
+
+                    server_capabilities = { semanticTokensProvider = false },
                 },
+            }
+
+            -- setup ocaml
+            require('ocaml').setup()
+
+            -- filter out servers with manual_install
+            local servers_to_install = vim.tbl_filter(function(key)
+                local t = servers[key]
+                if type(t) == 'table' then
+                    return not t.manual_install
+                else
+                    return t
+                end
+            end, vim.tbl_keys(servers))
+
+            require('mason').setup()
+
+            -- which servers should be autoinstalled by mason
+            local ensure_installed = {
+                'ts_ls',
+                'lua_ls',
+                'rust_analyzer',
+                'basedpyright',
+                'eslint',
+            }
+
+            -- run the mason installer
+            vim.list_extend(ensure_installed, servers_to_install)
+            require('mason-tool-installer').setup({
+                ensure_installed = ensure_installed,
             })
 
-            lsp_zero.set_sign_icons({
-                error = '✘',
-                warn = '▲',
-                hint = '⚑',
-                info = '',
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+            -- run lspconfig.setup() for each server
+            for name, config in pairs(servers) do
+                -- if config is true, then use default configuration
+                if config == true then config = {} end
+
+                -- use cmp capabilities
+                config = vim.tbl_deep_extend('force', {}, {
+                    capabilities = capabilities,
+                }, config)
+
+                lspconfig[name].setup(config)
+            end
+
+            -- run lsp setup when LSP is attached to buffer
+            vim.api.nvim_create_autocmd('LspAttach', {
+                callback = function(args)
+                    local bufnr = args.buf
+                    local client = assert(
+                        vim.lsp.get_client_by_id(args.data.client_id),
+                        'must have valid client'
+                    )
+
+                    local settings = servers[client.name]
+                    if type(settings) ~= 'table' then settings = {} end
+
+                    local opts = { buffer = bufnr, remap = false }
+                    vim.keymap.set(
+                        'n',
+                        'gd',
+                        require('telescope.builtin').lsp_definitions,
+                        opts
+                    )
+                    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+                    vim.keymap.set(
+                        'n',
+                        'gr',
+                        require('telescope.builtin').lsp_references,
+                        opts
+                    )
+                    vim.keymap.set(
+                        'n',
+                        'gI',
+                        require('telescope.builtin').lsp_implementations,
+                        opts
+                    )
+                    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+                    vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+
+                    vim.keymap.set(
+                        'n',
+                        '<leader>e',
+                        vim.diagnostic.open_float,
+                        opts
+                    )
+                    vim.keymap.set(
+                        'n',
+                        '<leader>q',
+                        vim.diagnostic.setloclist,
+                        opts
+                    )
+                    vim.keymap.set(
+                        'n',
+                        '<leader>vca',
+                        vim.lsp.buf.code_action,
+                        opts
+                    )
+                    vim.keymap.set(
+                        'n',
+                        '<leader>vrr',
+                        vim.lsp.buf.references,
+                        opts
+                    )
+                    vim.keymap.set(
+                        'n',
+                        '<leader>vrn',
+                        function()
+                            return ':IncRename ' .. vim.fn.expand('<cword>')
+                        end,
+                        { expr = true }
+                    )
+
+                    --- Insert mode - C-H to show signature
+                    vim.keymap.set(
+                        'i',
+                        '<C-h>',
+                        vim.lsp.buf.signature_help,
+                        opts
+                    )
+
+                    -- Override server capabilities
+                    if settings.server_capabilities then
+                        for k, v in pairs(settings.server_capabilities) do
+                            if v == vim.NIL then
+                                ---@diagnostic disable-next-line: cast-local-type
+                                v = nil
+                            end
+
+                            client.server_capabilities[k] = v
+                        end
+                    end
+                end,
             })
 
             vim.diagnostic.config({
@@ -239,7 +307,7 @@ return {
                 float = {
                     style = 'minimal',
                     border = 'rounded',
-                    source = 'always',
+                    source = true,
                     header = '',
                     prefix = '',
                     focusable = false,
