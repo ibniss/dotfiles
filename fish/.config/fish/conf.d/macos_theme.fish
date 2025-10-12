fe7ee138880b4eb4423180129987b8195edff4b5
Krzysztof Bielikowicz <krzysztof@causalens.com>
2025-10-12 21:26:02 +0100

    theme select

--- fish/.config/fish/conf.d/macos_theme.fish
+++ fish/.config/fish/conf.d/macos_theme.fish
@@ -1,9 +1,10 @@
+# selects correct tokyonight version based on macOS theme
 set -l os (uname)
 if test "$os" = "Darwin"
-    set -l theme (defaults read -g AppleInterfaceStyle)
-    # if test "$theme" = "Dark"
-    #     fish_config theme save "TokyoNight Moon"
-    # else
-    #     fish_config theme save "TokyoNight Day"
-    # end
+    set -l theme (defaults read -g AppleInterfaceStyle 2> /dev/null)
+    if test "$theme" = "Dark"
+        fish_config theme choose "TokyoNightMoon"
+    else
+        fish_config theme choose "TokyoNightDay"
+    end
 end
