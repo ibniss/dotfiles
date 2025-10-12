e3ef8c71004bb547d468562cc9f798d569d4de78
Krzysztof Bielikowicz <kbielikowicz98@gmail.com>
2025-10-12 21:26:02 +0100

    theme select

--- fish/.config/fish/conf.d/prompt.fish
+++ fish/.config/fish/conf.d/prompt.fish
@@ -1,5 +1,5 @@
-# set -g hydro_color_pwd blue
-# set -g hydro_color_prompt purple
-# set -g hydro_color_duration yellow
-# set -g hydro_color_git brblack
+set -g hydro_color_pwd blue
+set -g hydro_color_prompt purple
+set -g hydro_color_duration yellow
+set -g hydro_color_git brblack
 set -g hydro_multiline true
