cf43a8e8322a5395b24760ef3a312d245b62f7be
Krzysztof Bielikowicz <kbielikowicz98@gmail.com>
2025-10-13 01:04:53 +0100

    Override git colors in hydro prompt

--- fish/.config/fish/conf.d/prompt.fish
+++ fish/.config/fish/conf.d/prompt.fish
@@ -1,5 +1,15 @@
+# basic colors
 set -g hydro_color_pwd blue
 set -g hydro_color_prompt purple
 set -g hydro_color_duration yellow
 set -g hydro_color_git brblack
+
+# override colors for git symbols
+set -g hydro_color_git_dirty magenta
+set -g hydro_color_git_ahead cyan
+set -g hydro_color_git_behind cyan
+set -g hydro_symbol_git_ahead "$_hydro_color_git_ahead↑$hydro_color_normal"
+set -g hydro_symbol_git_behind "$_hydro_color_git_behind↓$hydro_color_normal"
+set -g hydro_symbol_git_dirty "$_hydro_color_git_dirty*$hydro_color_normal"
+
 set -g hydro_multiline true
