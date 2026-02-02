f272d0466c4b0fb6f41186fcc279b5413bb2dc75
Krzysztof Bielikowicz <krzysztof@causalens.com>
2026-02-02 00:50:52 +0000

    Upgrade treesitter setup to use newest APIs, use mini.ai for better around/inside textobjects

--- nvim/.config/nvim/lua/ibniss/plugins/mini.lua
+++ nvim/.config/nvim/lua/ibniss/plugins/mini.lua
@@ -1,4 +1,29 @@
 return { -- Collection of various small independent plugins/modules
-  "echasnovski/mini.nvim",
-  config = function() require("mini.surround").setup {} end,
+  "nvim-mini/mini.nvim",
+  config = function()
+    -- Better Around/Inside textobjects
+    --
+    -- Examples:
+    --  - va)  - [V]isually select [A]round [)]paren
+    --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
+    --  - ci'  - [C]hange [I]nside [']quote
+    local gen_spec = require("mini.ai").gen_spec
+    require("mini.ai").setup {
+      n_lines = 500,
+      custom_textobjects = {
+        -- Tweak function call to not detect dot in function name
+        f = gen_spec.function_call { name_pattern = "[%w_]" },
+        -- NOTE: those are using queries coming from nvim-treesitter-textobjects
+        F = gen_spec.treesitter { a = "@function.outer", i = "@function.inner" },
+        c = gen_spec.treesitter { a = "@class.outer", i = "@class.inner" },
+      },
+    }
+
+    -- Add/delete/replace surroundings (brackets, quotes, etc.)
+    --
+    -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+    -- - sd'   - [S]urround [D]elete [']quotes
+    -- - sr)'  - [S]urround [R]eplace [)] [']
+    require("mini.surround").setup {}
+  end,
 }
--- nvim/.config/nvim/lua/ibniss/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/ibniss/plugins/treesitter.lua
@@ -1,82 +1,68 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    branch = "main",
     build = ":TSUpdate",
     config = function()
-      require("nvim-treesitter.configs").setup {
-        -- A list of parser names, or "all" (the five listed parsers should always be installed)
-        ensure_installed = {
-          "diff",
-          "javascript",
-          "typescript",
-          "tsx",
-          "python",
-          "rust",
-          "c",
-          "css",
-          "html",
-          "lua",
-          "vim",
-          "vimdoc",
-          "query",
-          "regex",
-          "bash",
-          "markdown",
-          "markdown_inline",
-        },
-        ignore_install = {},
-        modules = {},
-        filetype_to_parsername = {
-          mdx = "markdown",
-        },
-
-        -- Install parsers synchronously (only applied to `ensure_installed`)
-        sync_install = false,
-
-        -- Automatically install missing parsers when entering buffer
-        -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-        auto_install = true,
-        indent = {
-          enable = false,
-        },
-
-        highlight = {
-          enable = true,
-
-          -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-          -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-          -- Using this option may slow down your editor, and you may see some duplicate highlights.
-          -- Instead of true it can also be a list of languages
-          additional_vim_regex_highlighting = false,
-        },
-        textobjects = {
-          select = {
-            enable = true,
-            keymaps = {
-              ["af"] = "@function.outer",
-              ["if"] = "@function.inner",
-              ["ac"] = "@class.outer",
-              ["ic"] = "@class.inner",
-            },
-          },
-        },
+      local filetypes = {
+        "diff",
+        "javascript",
+        "typescript",
+        "tsx",
+        "python",
+        "rust",
+        "c",
+        "css",
+        "html",
+        "lua",
+        "luadoc",
+        "vim",
+        "vimdoc",
+        "query",
+        "regex",
+        "bash",
+        "markdown",
+        "markdown_inline",
       }
+
+      -- Install parsers using native API
+      require("nvim-treesitter").install(filetypes)
+
+      -- Enable highlighting via native API
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = filetypes,
+        callback = function() vim.treesitter.start() end,
+      })
+
+      -- Handle mdx -> markdown parser
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "mdx",
+        callback = function() vim.treesitter.start(0, "markdown") end,
+      })
     end,
   },
   {
     "nvim-treesitter/nvim-treesitter-context",
     dependencies = { "nvim-treesitter/nvim-treesitter" },
-    config = function()
-      local tsc = require "treesitter-context"
-
-      tsc.setup {
-        enable = true,
-        max_lines = 1,
-        trim_scope = "inner",
-      }
-    end,
+    opts = {
+      enable = true,
+      max_lines = 1,
+      trim_scope = "inner",
+    },
   },
+  -- NOTE: using textobjects just for their built-in queries, actual bindings are used by mini.ai
   {
     "nvim-treesitter/nvim-treesitter-textobjects",
+    branch = "main",
+    config = function()
+      local select = require "nvim-treesitter-textobjects.select"
+
+      require("nvim-treesitter-textobjects").setup {
+        select = {
+          lookahead = true,
+        },
+      }
+    end,
   },
 }
