989b26a12d14f8aede6d7da996129835b36c0a89
Krzysztof Bielikowicz <krzysztof@causalens.com>
2026-02-07 14:38:56 +0000

    Tweak fish non-interactive config

--- fish/.config/fish/conf.d/mise.fish
+++ fish/.config/fish/conf.d/mise.fish
@@ -1,8 +1,8 @@
 if command -q mise then
-    if status is-login
-        mise activate fish --shims | source
+    mise activate fish --shims | source
+    if status is-interactive
+        mise activate fish | source
     end
-    mise activate fish | source
 else if test -d "$HOME/.local/bin/mise"; then
     $HOME/.local/bin/mise activate fish | source
 end
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,14 +1,11 @@
 # disables fish greeting
 set fish_greeting
 
-# login-only (ZSH equivalent of ~/.zprofile)
-if status is-login
-    fish_add_path "$HOME/.local/bin"
-end
 
 # -----------------------------------------------------------------------------
 # Common PATH Exports
 # -----------------------------------------------------------------------------
+fish_add_path "$HOME/.local/bin"
 fish_add_path "$HOME/bin" "/usr/local/bin" "/usr/bin" "/bin"
 fish_add_path "/usr/local/sbin" "/usr/sbin" "/sbin"
 fish_add_path "~/.npm-global/bin"
